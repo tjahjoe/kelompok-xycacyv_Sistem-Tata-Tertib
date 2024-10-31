cea6d9c36974ef3bbd9613b8f2712a7bd8f86d46
tjahjoe <ntell634@gmail.com>
2024-10-31 17:31:05 +0700

    logic login

--- app/controllers/AuthLogin.php
+++ app/controllers/AuthLogin.php
@@ -1,5 +1,30 @@
 <?php
+session_start();
+
+header('Content-Type: application/json');
+
 require_once __DIR__ . "/../models/User.php";
-$user = new User();
-$user->login("xycacyv", "ok");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $userModel = new User();
+
+    $response = [
+        'status' => 'error',
+        'message' => 'Email or Password is incorrect!',
+    ];
+
+    $id = $_POST['idAnggota'];
+    $password = $_POST['password'];
+
+    $user = $userModel->login($id, $password);
+
+    if ($user) {
+        $response['status'] = 'success';
+        $response['message'] = 'Login successful';
+        $_SESSION['user'] = $user;
+    }
+
+    echo json_encode($response);
+    exit;
+}
 ?>
