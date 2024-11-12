3ab8c6427956f6aa31acc3f0af5d4986db30e3d4
tjahjoe <ntell634@gmail.com>
2024-11-12 14:15:10 +0700

    add handler for daftar pelaporan

---
+++ app/controllers/FilterDaftarPelaporan.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . "/../models/PelanggaranMahasiswa.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_SESSION['user'])) {
+    $pelanggaranMahasiswaModel = new PelanggaranMahasiswa();
+
+    $response = [
+        'status' => 'error',
+        'message' => 'No data found for the selected tingkat pelanggaran.',
+    ];
+
+    $nim = $_POST[''];
+    $tanggalAwal = $_POST[''];
+    $tanggalAkhir = $_POST[''];
+    $tingkat = $_POST[''];
+    $status = $_POST[''];
+
+    $results = $pelanggaranMahasiswaModel->getDaftarPelaporan(
+        $nim,
+        $tanggalAwal,
+        $tanggalAkhir,
+        $tingkat,
+        $status
+    );
+
+    echo $results ? json_encode(['status' => 'success', 'data' => $results]) : json_encode($response);
+    exit;
+
+}
+?>
